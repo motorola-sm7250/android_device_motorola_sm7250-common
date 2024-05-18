66c7785da4518e23c4b8c80c992c89d8e10bdde6
Bruno Martins <bgcngm@gmail.com>
2024-05-18 13:21:42 +0100

    sm7250-common: Use common libqti-perfd-client and power-libperfmgr
    Change-Id: I22d6b35c72629553817005deaefcae60abc72cfd

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -227,6 +227,7 @@ TARGET_RIL_VARIANT := caf
 
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
 
